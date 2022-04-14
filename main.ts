8a5d3e9032dfef474a72aa0c95183a654e952321
jjunnnys <salswnss@gmail.com>
2022-04-15 02:56:20 +0900

    🌱 setting

---
+++ main.ts
@@ -0,0 +1,11 @@
+{
+  const users = [
+    { id: 1, name: "Id1", age: 32 },
+    { id: 2, name: "HA", age: 25 },
+    { id: 3, name: "BJ", age: 32 },
+    { id: 4, name: "PJ", age: 28 },
+    { id: 5, name: "JE", age: 27 },
+    { id: 6, name: "JM", age: 32 },
+    { id: 7, name: "HI", age: 24 },
+  ];
+}
